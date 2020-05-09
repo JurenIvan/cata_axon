d9c97c49ffecce2260c327f4215db178fc11c0eb
jurenivan <juren.ivan@gmail.com>
2020-05-09 11:16:26 +0200

    initial commit

---
+++ email-microservice/src/main/java/hr/fer/cata/EmailsApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.cata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailsApplication.class, args);
+	}
+
+}
