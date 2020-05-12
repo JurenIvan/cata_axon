82ee9d4c53cac6773f34e00a56e1f2dfc3a368e9
jurenivan <juren.ivan@gmail.com>
2020-05-12 14:50:28 +0200

    add EndpointsDefinitions interface

--- eureka-gateway/src/main/java/hr/fer/gateway/interfaces/AuthREST.java
+++ eureka-gateway/src/main/java/hr/fer/gateway/interfaces/AuthREST.java
@@ -1,7 +1,18 @@
 package hr.fer.gateway.interfaces;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("cata-auth")
 public interface AuthREST {
+
+    @PostMapping(value = "/authenticate")
+    ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequestDto authenticationRequest);
+
+    @GetMapping("/current-roles")
+    List<Role> getCurrentRoles();
+
 }
--- eureka-gateway/src/main/java/hr/fer/gateway/interfaces/TripsREST.java
+++ eureka-gateway/src/main/java/hr/fer/gateway/interfaces/TripsREST.java
@@ -1,13 +1,34 @@
 package hr.fer.gateway.interfaces;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("cata-trips")
 public interface TripsREST {
 
-    @RequestMapping("/trip/{applicationName}")
-    String example1(@PathVariable String applicationName);
+    @PostMapping("/create-trip")
+    void createTrip(@RequestBody TripDto tripDto);
 
+    @PostMapping("/join-trip")
+    void joinTrip(@RequestBody UserTrip userTrip);
+
+    @PostMapping("/leave-trip")
+    void leaveTrip(@RequestBody UserTrip userTrip);
+
+    @PostMapping("/approve")
+    void approveTrip(@RequestBody UserTrip userTrip);
+
+    @PostMapping("/dissmiss")
+    void dismissTrip(@RequestBody UserTrip userTrip);
+
+    @PostMapping("/accept-user")
+    void acceptUserToTrip(UserTrip userTrip);
+
+    @PostMapping("/deny-user")
+    void denyUserToTrip(UserTrip userTrip);
+
+    @GetMapping("/view-trips")
+    List<TripDto> viewTrips();
 }
