1617c7a14af5896914ff208f88a3e7d79646d171
jurenivan <juren.ivan@gmail.com>
2020-05-26 01:01:30 +0200

    add feignClient to TripsApplication

--- connector/src/main/java/hr/fer/connector/interfaces/InternalAuthREST.java
+++ connector/src/main/java/hr/fer/connector/interfaces/InternalAuthREST.java
@@ -1,13 +1,8 @@
 package hr.fer.connector.interfaces;
 
-import hr.fer.connector.dto.auth.JWTokenDto;
-import hr.fer.connector.dto.auth.PersonDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 //@FeignClient("cata-auth")
-public interface InternalAuthREST {
-
-    @PostMapping("/auth/jwt")
-    public PersonDto getUserForJWT(@RequestBody JWTokenDto jwTokenDto);
-}
+//public interface InternalAuthREST {
+//
+//    @PostMapping("/auth/jwt")
+//    public PersonDto getUserForJWT(@RequestBody JWTokenDto jwTokenDto);
+//}
--- trips/src/main/java/hr/fer/cata/trips/TripsController.java
+++ trips/src/main/java/hr/fer/cata/trips/TripsController.java
@@ -1,6 +1,9 @@
 package hr.fer.cata.trips;
 
+import hr.fer.connector.dto.auth.RegisterRequestDto;
+import hr.fer.connector.interfaces.AuthREST;
 import hr.fer.connector.interfaces.TripsREST;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TripsController implements TripsREST {
 
+    @Autowired
+    private AuthREST authREST;
+
     @Override
     @RequestMapping("/trip/{applicationName}")
     public String example1(@PathVariable String applicationName) {
+        authREST.registerUser(new RegisterRequestDto("asda.sad@asd.as", "asdasdasda", "sadasdasd"));
         System.out.println("example1 at TripsController");
         return "path returneed " + applicationName;
     }
